ffed70f192590e2f998c40e3d8b76336c924c342
Heitor Cruz <111433592+HeitorSC@users.noreply.github.com>
2024-03-18 21:56:51 -0300

    Criada a primeira classe de dominio - Categoria

--- src/main/java/com/heitor/mendes/resources/CategoriaResource.java
+++ src/main/java/com/heitor/mendes/resources/CategoriaResource.java
@@ -1,15 +1,29 @@
 package com.heitor.mendes.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "<h1>Rest está funcionando mesmo</h1>";
+	public List<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(2, "Escritório");
+		
+		List<Categoria> list = new ArrayList<>();
+		
+		list.add(cat1);
+		list.add(cat2);
+			
+		return list;
 	}
 }
---
+++ src/main/java/domain/Categoria.java
@@ -0,0 +1,71 @@
+package domain;
+
+import java.util.Objects;
+import java.io.Serializable;
+
+public class Categoria implements Serializable{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private  Integer id;
+	private String name;
+	
+	public Categoria() {}
+	
+	
+	public Categoria (Integer id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Categoria other = (Categoria) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
